f69a18579e47c24ad6b2f7e79a10be7ed8116389
pushpendra paliwal <pushpendra@hubilo.com>
2023-01-16 12:11:14 +0530

    fiest commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FastboardComponent } from './Fastboard';
+
+const SDK_TOKEN = process.env.SDK_TOKEN ?? '';
+const REGION = 'in-mum';
+
+const createRoomOption = {
+  method: 'POST',
+  headers: {
+    token: SDK_TOKEN,
+    'Content-Type': 'application/json',
+    region: REGION,
+  },
+  body: JSON.stringify({
+    isRecord: false,
+  }),
+};
+
+const tokenOption = {
+  method: 'POST',
+  headers: {
+    token: SDK_TOKEN,
+    'Content-Type': 'application/json',
+    region: REGION,
+  },
+  body: JSON.stringify({ lifespan: 3600000, role: 'admin' }),
+};
 
 function App() {
-  return (
-    <div className="App">
+  const [userId] = React.useState(parseInt(`${Math.random() * 1e6}`) + '');
+  const [roomId, setRoomId] = React.useState<string | undefined>(undefined);
+  const [roomToken, setRoomToken] = React.useState<string | undefined>(undefined);
+
+  async function createRoom() {
+    try {
+      const res = await fetch(`https://api.netless.link/v5/rooms`, createRoomOption);
+      const data = await res.json();
+      console.log('response of the room create',data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function createToken(uuid: string) {
+    try {
+      const res = await fetch(`https://api.netless.link/v5/tokens/rooms/${uuid}`, tokenOption);
+      const data = await res.json();
+      console.log('response of the token',data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  React.useEffect(() => {
+    createRoom()
+      .then((response) => {
+        console.log('@@@@ response create room >', response);
+        setRoomId(response.uuid);
+        return createToken(response.uuid);
+      })
+      .then((res) => {
+        console.log('@@@@ token >>>', res);
+        setRoomToken(res);
+      });
+  }, []);
+
+  if (typeof roomId === 'undefined' || typeof roomToken === 'undefined') {
+    return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+    );
+  }
+
+  return (
+    <div className="App">
+      <FastboardComponent roomId={roomId} userId={userId} roomToken={roomToken} />
     </div>
   );
 }
